2a47e5a43dab5cbbcfb07683284be9f56b94dc57
amayaschalmers <amary@student.chalmers.se>
2025-11-28 01:03:16 +0100

    View_update with Theme class

--- src/main/java/View/Theme.java
+++ src/main/java/View/Theme.java
@@ -2,11 +2,11 @@ package View;
 
 import java.awt.*;
 public class Theme {
-    public static final Color BG = new Color(255,242,213);
+    public static final Color BG = new Color(81,76,73);
     // Nave bar background color
-    public static final Color NAV_BG = new Color(250, 235, 200);
-    public static final Color CARD_BG = new Color(255, 240, 150);
-    public static final Color TEXT = new Color(60, 60, 60);
+    public static final Color NAV_BG = new Color(81,76,73);
+    public static final Color CARD_BG = new Color(255,255,255);
+    public static final Color TEXT = new Color(255,255,255);
 
     public static final Font TITLE = new Font("SansSerif", Font.BOLD, 18);
     public static final Font NORMAL = new Font("SansSerif", Font.PLAIN, 14);
