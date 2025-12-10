da2292d189301305fb91346c78da1c3c359ae554
Albin Jonsson <96695909+AlbinJonsson@users.noreply.github.com>
2025-12-10 19:08:19 +0100

    Added JUnit test dependency

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleTest {
+
+    @Test
+    void shouldAddCorrectly() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}
